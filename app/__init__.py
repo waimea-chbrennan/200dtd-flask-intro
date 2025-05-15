a5f50f16365ef254a03776b8d91547c0b1e9f655
Connor Brennan <chbrennan@waimea.school.nz>
2025-05-15 12:09:19 +1200

    add jinja variables and css

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from random import randint
+from sys import *
 
 # Create the app
 app = Flask(__name__)
@@ -13,8 +14,15 @@ def home():
 
 @app.get("/random")
 def random():
-    return str(randint(1,2**1024))
+    set_int_max_str_digits(100000000)
+    randNumber = randint(1,2**2**16)
+    return render_template('pages/random.jinja', number=randNumber)
 
 @app.get("/about")
 def about():
     return render_template('pages/about.jinja')
+
+
+@app.errorhandler(404)
+def notFound(error):
+    return "404: page not found :("

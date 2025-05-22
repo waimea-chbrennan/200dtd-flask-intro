0ea4a575dcbdf156f8f35f1bef7d90ce207c4941
Connor Brennan <chbrennan@waimea.school.nz>
2025-05-22 12:25:29 +1200

    create pattern matching roots and form

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from random import randint
+from flask import redirect
 from sys import *
 
 # Create the app
@@ -12,17 +13,29 @@ app = Flask(__name__)
 def home():
     return render_template('pages/home.jinja')
 
+@app.get("/random/")
 @app.get("/random")
 def random():
     set_int_max_str_digits(100000000)
     randNumber = randint(1,2**2**16)
     return render_template('pages/random.jinja', number=randNumber)
 
+@app.get("/number/<int:num>/")
+@app.get("/number/<int:num>")
+def analyseNumber(num):
+    print(f"You entered: {num}")
+    return render_template('pages/number.jinja',number=num)
+
+@app.get("/about/")
 @app.get("/about")
 def about():
     return render_template('pages/about.jinja')
 
+@app.get("/form/")
+@app.get("/form")
+def form():
+    return render_template('pages/form.jinja')
 
 @app.errorhandler(404)
 def notFound(error):
-    return "404: page not found :("
+    return render_template('pages/404.jinja')
